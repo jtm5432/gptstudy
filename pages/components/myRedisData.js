1fe8c2d99722fe73e71bd01795fae2a923259a42
Oracle Public Cloud User <opc@instance-20230423-1746.sub04230625530.studyvcn.oraclevcn.com>
2023-06-01 13:18:16 +0000

    starpage에서 서버사이드 랜더링인데 axios로 데이터 받아오는 부분을 redis에서 바로 받아오게 변경

--- pages/components/myRedisData.js
+++ pages/components/myRedisData.js
@@ -2,12 +2,14 @@
 import React from 'react';
 
 const RenderRedis = ({ data }) => {
-  console.log(data)
+  console.log('renderRedis', data)
   return (
     <div>
       <h1>Redis Application</h1>
       <ul>
-      
+        {data?.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
       </ul>
     </div>
   );
--- pages/starpage.js
+++ pages/starpage.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 import RenderRedis from './components/myRedisData.js';
+import myRedis from './api/lib/redisClient.js';
 
 const HomePage = ({ data }) => {
   return <RenderRedis data={data} />
 };
 
 export async function getServerSideProps() {
-  const response = await fetch('http://localhost:4000/api/renderRedis');
-
-  if (!response.ok) {
-    return {
-      notFound: true,
-    };
+  //서버사이드 랜더링인데 axios같은거 쓸 필요가 없다.
+  //const response = await fetch('http://localhost:4000/api/renderRedis');
+  let response;
+  try {
+     response  = await myRedis.getKeyArray('*');
+  } catch (E){
+    console.log('Error',E);
   }
+  
 
-  let data = await response.json();
-  console.log(data);
+  let data = response;
+  console.log('data',data);
   if(!data ) data = [];
+ // else data = data.map((e) => e.replaceAll('\n',''))
   return {
     props: { data }, // will be passed to the page component as props
   };
